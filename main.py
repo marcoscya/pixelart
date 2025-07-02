5f21faafb1f954ef6403f92a0b6ff5982f3c07be
Marcos <markus080904@gmail.com>
2025-07-01 19:32:16 -0500

    mostrar animación flotante con fondo transparente sobre el escritorio usando pywin32

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import pygame as pg
+import ctypes
+import win32gui
+import win32con
+import win32api
+
+
 from character import Character
 from config import *
 
 pg.init()
-pantalla = pg.display.set_mode((ANCHO, ALTO))
+pantalla = pg.display.set_mode((ANCHO, ALTO), pg.NOFRAME)
 pg.display.set_caption("Pacman Pixel Art")
+
+hwnd = pg.display.get_wm_info()['window']
+
+win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 100, 100, 0, 0,
+                        win32con.SWP_NOSIZE | win32con.SWP_NOACTIVATE)
+extended_style = win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE)
+
+win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
+                        extended_style | win32con.WS_EX_LAYERED)
+
+win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(30, 30, 30), 0,
+                                    win32con.LWA_COLORKEY)
 
 character = Character("assets/pacman.png", 90, 116)
 
